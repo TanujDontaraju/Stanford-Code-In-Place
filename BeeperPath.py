b373f58ffcb1482fd69701f1c7123e1096b32872
Tanuj Dontaraju <165433556+TanujDontaraju@users.noreply.github.com>
2025-04-23 20:03:58 -0400

    Create BeeperPath.py

---
+++ BeeperPath.py
@@ -0,0 +1,13 @@
+# Link to veiw project: https://codeinplace.stanford.edu/cip5/share/qToU1dTsRFbQlnVQDzDQ
+
+from karel.stanfordkarel import *
+
+def main():
+    # While loop to make it work in any situation
+    while beepers_present():
+        move()
+
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
