5e78f5539d7f948fd61c8a6af1e0ab3af67be5f3
Tanuj Dontaraju <165433556+TanujDontaraju@users.noreply.github.com>
2025-05-04 13:04:08 -0400

    Create HelloName.py

---
+++ HelloName.py
@@ -0,0 +1,13 @@
+# Link to view project: https://codeinplace.stanford.edu/cip5/share/AUv0RVQCjEn3wL1mkyYn
+
+# Main Function 
+def main():
+    # Input name
+    name = input("What is your name? ")
+    # Prints hello and inputted name 
+    # Use f"Hello {name}" if "Hello {name}" doesn't work inside print statement
+    print(f"Hello {name}")
+    
+# No changes from here
+if __name__ == '__main__':
+    main()
