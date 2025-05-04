c7037049a91eab0305f6aac8c99a1f0f9e45a1d9
Tanuj Dontaraju <165433556+TanujDontaraju@users.noreply.github.com>
2025-05-04 13:19:06 -0400

    Create Sunrise-Fill-In-Blanks.py

---
+++ Sunrise-Fill-In-Blanks.py
@@ -0,0 +1,16 @@
+# Link to view project: https://codeinplace.stanford.edu/cip5/share/LSObQLocdScvwDOeVbvM
+
+# Main Function
+def main():
+    # Input for color
+    color = input("A color: ")
+    # Input for adjective
+    adjective = input("An adjective: ")
+    # Input for adjective 
+    goal = input("A goal you would like to achieve: ")
+    # Use "f" if print statement doesn't work properly
+    print(f"At dawn the sky turned {color}, and the air felt {adjective}. I decided today I will finally {goal}.")
+
+# No changes from here 
+if __name__ == "__main__":
+    main()
